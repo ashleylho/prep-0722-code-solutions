36436587bec0bc76e9f1b36c2cc3180c3f4b1271
Ashley Ho <ashley.lho@gmail.com>
2022-08-12 01:29:41 +0000

    completed prep-javascript-for-loops exercise

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+// create your loops here.
+
+for (var i = 0; i < 10; ++i) {
+  console.log(i);
+}
+
+for (var j = 0; j <= 18; j += 2) {
+  console.log(j);
+}
+
+for (var a = 100; a >= 0; a--) {
+  console.log('Time till explosion:', a);
+}
